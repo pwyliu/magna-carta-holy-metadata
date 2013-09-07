b90faa1c8904ffee224a3e3c45791a65d5823934
pliu <paul_liu@kanetix.ca>
2013-09-07 04:14:36 -0400

    add build_index.py to startup

--- conf/start_gunicorn.sh
+++ conf/start_gunicorn.sh
@@ -15,5 +15,6 @@ VENV=venv/bin/activate
 
 cd $APPPATH
 source $VENV
+python mchm/build_index.py
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn -b 127.0.0.1:$PORT -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE runserver:app
