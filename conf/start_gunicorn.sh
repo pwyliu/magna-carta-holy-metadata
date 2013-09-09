1172b97d516b97d9d35786f47ccaa4a45daf5551
pliu <paul_liu@kanetix.ca>
2013-09-09 14:17:53 -0400

    change log path

--- conf/start_gunicorn.sh
+++ conf/start_gunicorn.sh
@@ -4,8 +4,7 @@ set -e
 
 
 APPPATH=/opt/magna-carta-holy-metadata
-LOGFILE=/opt/magna-carta-holy-metadata/gunicorn.log
-LOGDIR=$(dirname $LOGFILE)
+LOGFILE=$APPPATH/gunicorn.log
 
 PORT=8000
 NUM_WORKERS=2
@@ -17,5 +16,4 @@ VENV=venv/bin/activate
 cd $APPPATH
 source $VENV
 python mchm/build_index.py
-test -d $LOGDIR || mkdir -p $LOGDIR && chown $USER:$GROUP $LOGDIR
 exec gunicorn -b 127.0.0.1:$PORT -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE runserver:app
