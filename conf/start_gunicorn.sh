529000b286997e91bce826704f14a78cea64b6d3
pliu <paul_liu@kanetix.ca>
2013-09-09 14:10:17 -0400

    change log path

--- conf/start_gunicorn.sh
+++ conf/start_gunicorn.sh
@@ -2,7 +2,9 @@
 # Stolen from http://senko.net/en/django-nginx-gunicorn/
 set -e
 
-LOGFILE=/var/log/gunicorn/mchm.log
+
+APPPATH=/opt/magna-carta-holy-metadata
+LOGFILE=/opt/magna-carta-holy-metadata/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 
 PORT=8000
@@ -10,11 +12,10 @@ NUM_WORKERS=2
 
 USER=www-data
 GROUP=www-data
-APPPATH=/opt/magna-carta-holy-metadata
 VENV=venv/bin/activate
 
 cd $APPPATH
 source $VENV
 python mchm/build_index.py
-test -d $LOGDIR || mkdir -p $LOGDIR
+test -d $LOGDIR || mkdir -p $LOGDIR && chown $USER:$GROUP $LOGDIR
 exec gunicorn -b 127.0.0.1:$PORT -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE runserver:app
