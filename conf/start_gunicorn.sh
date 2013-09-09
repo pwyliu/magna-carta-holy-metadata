31a76eb7491e11379c515fb6cff1a197b37e8122
pliu <paul_liu@kanetix.ca>
2013-09-09 14:27:47 -0400

    change log path, chmod +x

---
+++ conf/start_gunicorn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Stolen from http://senko.net/en/django-nginx-gunicorn/
+set -e
+
+USER=www-data
+GROUP=www-data
+VENV=venv/bin/activate
+
+APPPATH=/opt/magna-carta-holy-metadata
+LOGFILE=$APPPATH/gunicorn.log
+
+PORT=8000
+NUM_WORKERS=2
+
+cd $APPPATH
+source $VENV
+python mchm/build_index.py
+exec gunicorn -b 127.0.0.1:$PORT -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE runserver:app
